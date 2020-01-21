3206b16aecabd854d8fe6bad86ff1cb1b0dc6021
pujan <pujanov69@gmail.com>
2020-01-21 11:45:51 +0545

    Project Initialization

---
+++ src/test/java/com/ishanitech/ipalika/IpalikaFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ishanitech.ipalika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpalikaFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
