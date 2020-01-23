3cfc9f5f9e29b63392c3257aa851c79e997de767
Umesh Bhujel <yoomesbhujel@gmail.com>
2020-01-23 11:47:57 +0545

    added admin controller and it's view

---
+++ src/main/java/com/ishanitech/ipalikawebapp/controller/admin/AdminController.java
@@ -0,0 +1,16 @@
+package com.ishanitech.ipalikawebapp.controller.admin;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RequestMapping("/admin")
+@Controller
+public class AdminController {
+	@GetMapping
+	public String getDashboardView() {
+		return "admin/dashboard";
+	}
+}
