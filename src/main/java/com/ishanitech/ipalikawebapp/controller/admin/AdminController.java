2ed8f8c5946604fe4fae193f480b3642d1cd9550
Tanchhowpa Singzango <revx.17@gmail.com>
2020-01-24 12:37:18 +0545

    adde additional views

--- src/main/java/com/ishanitech/ipalikawebapp/controller/admin/AdminController.java
+++ src/main/java/com/ishanitech/ipalikawebapp/controller/admin/AdminController.java
@@ -13,4 +13,40 @@ public class AdminController {
 	public String getDashboardView() {
 		return "admin/dashboard";
 	}
+	
+//	@GetMapping("/dataInsert")
+//	public String getDataEntry() {
+//		return "admin/data-insert";
+//	}
+	
+	@GetMapping("/addHouseholdForm")
+	public String getHouseholdEntryForm() {
+		return "admin/add-household";
+	}
+	
+	@GetMapping("/addMemberForm")
+	public String getMemberEntryForm() {
+		return "admin/add-member";
+	}
+	
+	@GetMapping("/userSettings") 
+	public String getUserSettingsView() {
+		return "admin/user-settings";
+	}
+	
+	@GetMapping("/userProfile")
+	public String getUserProfileView() {
+		return "admin/user-profile";
+	}
+	
+	@GetMapping("/residentData")
+	public String getResidentDataList() {
+		return "admin/resident-data";
+	}
+	
+	@GetMapping("/residentMembers")
+	public String getResidentMemberList() {
+		return "admin/resident-members";
+	}
+	
 }
