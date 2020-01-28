603d871ac4aba000b67340b0155452345f92f485
Tanchhowpa Singzango <revx.17@gmail.com>
2020-01-28 11:32:14 +0545

    fixed minor texts and logos

--- src/main/java/com/ishanitech/ipalikawebapp/controller/admin/AdminController.java
+++ src/main/java/com/ishanitech/ipalikawebapp/controller/admin/AdminController.java
@@ -61,7 +61,7 @@ public class AdminController {
 	public String getResidentMemberList(@PathVariable("filledFormId") String filledId, Model model) {
 		Response<AnswerDTO> residentResponse = (Response<AnswerDTO>) residentService.getResidentFullDetail(filledId);
 		model.addAttribute("residentFullDetail", residentResponse.getData());
-		return "admin/resident-members";
+		return "admin/resident-details";
 	}
 
 }
