7444806a69b02e6c1d868340a94c45cb8a30f4cf
Tanchhowpa Singzango <revx.17@gmail.com>
2020-02-09 19:02:32 +0545

    added feature to add individual member

--- src/main/java/com/ishanitech/ipalikawebapp/service/ResidentService.java
+++ src/main/java/com/ishanitech/ipalikawebapp/service/ResidentService.java
@@ -1,10 +1,12 @@
 package com.ishanitech.ipalikawebapp.service;
 
 
+import com.ishanitech.ipalikawebapp.dto.FamilyMemberDTO;
 import com.ishanitech.ipalikawebapp.dto.Response;
 
 public interface ResidentService {
 
 	Response<?> getResidentDataList();
 	Response<?> getResidentFullDetail(String filledId);
+	void addFamilyMember(FamilyMemberDTO familyMemberInfo);
 }
--- src/main/java/com/ishanitech/ipalikawebapp/serviceImpl/ResidentServiceImpl.java
+++ src/main/java/com/ishanitech/ipalikawebapp/serviceImpl/ResidentServiceImpl.java
@@ -2,14 +2,20 @@ package com.ishanitech.ipalikawebapp.serviceImpl;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 import org.springframework.web.client.RestTemplate;
 
+import com.ishanitech.ipalikawebapp.dto.FamilyMemberDTO;
 import com.ishanitech.ipalikawebapp.dto.ResidentDTO;
 import com.ishanitech.ipalikawebapp.dto.ResidentDetailDTO;
 import com.ishanitech.ipalikawebapp.dto.Response;
 import com.ishanitech.ipalikawebapp.service.ResidentService;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class ResidentServiceImpl implements ResidentService {
 
@@ -31,6 +37,13 @@ public class ResidentServiceImpl implements ResidentService {
 	public Response<?> getResidentFullDetail(String filledId) {
 		Response<ResidentDetailDTO> fullDetail = restTemplate.getForObject("http://localhost:8888/resident/detail/" + filledId, Response.class);
 		return fullDetail;
+	}
+
+	@Override
+	public void addFamilyMember(FamilyMemberDTO familyMemberInfo) {
+		log.info(familyMemberInfo.toString());
+		restTemplate.postForObject("http://localhost:8888/resident/member/", familyMemberInfo, String.class);
+		
 	}
 
 }
