a1fa9d0556283c8dc067258386d66f4b9b3e880a
pujanov69 <pujanov69@gmail.com>
2020-02-28 17:34:40 +0545

    fixed unused imports

--- src/main/java/com/ishanitech/ipalikawebapp/controller/admin/SuperAdminController.java
+++ src/main/java/com/ishanitech/ipalikawebapp/controller/admin/SuperAdminController.java
@@ -1,13 +1,9 @@
 package com.ishanitech.ipalikawebapp.controller.admin;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.ishanitech.ipalikawebapp.dto.AnswerDTO;
-import com.ishanitech.ipalikawebapp.dto.UserDTO;
 import com.ishanitech.ipalikawebapp.service.FormService;
 
 import lombok.extern.slf4j.Slf4j;
