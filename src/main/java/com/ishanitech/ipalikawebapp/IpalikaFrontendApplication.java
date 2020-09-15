3fdf3d8d121eb96fb6dfd705ee3b44e83b991338
pujanov69 <pujanov69@gmail.com>
2020-09-15 22:54:31 +0545

    ssl support for webapp

--- src/main/java/com/ishanitech/ipalikawebapp/IpalikaFrontendApplication.java
+++ src/main/java/com/ishanitech/ipalikawebapp/IpalikaFrontendApplication.java
@@ -2,9 +2,17 @@ package com.ishanitech.ipalikawebapp;
 
 import java.util.Arrays;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -15,6 +23,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 public class IpalikaFrontendApplication {
+	
+	@Value("${server.port}")
+	private int httpPort;
 
 	public static void main(String[] args) {
 		SpringApplication.run(IpalikaFrontendApplication.class, args);
@@ -57,6 +68,34 @@ public class IpalikaFrontendApplication {
     	factory.setReadTimeout(5000);
     	return factory;
     }
+	
+	
+	// Lets configure additional connector to enable support for both HTTP and HTTPS
+	 	@Bean
+	 	@Profile("prod")
+	 	public ServletWebServerFactory servletContainer() {
+	 		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+	 		tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+	 		return tomcat;
+	 	}
+
+	 	private Connector createStandardConnector() {
+	 		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+	 		connector.setScheme("http");
+	 		connector.setPort(httpPort);
+	 		connector.setSecure(false);
+	 		connector.setRedirectPort(443);
+	 		return connector;
+	 	}
 	
 
 }
